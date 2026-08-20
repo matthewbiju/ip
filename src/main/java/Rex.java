39f3b589ba9c56976d179420edc9ba436772975f
Matthew Biju George <matthewbiju24@gmail.com>
2026-08-21 01:51:27 +0800

    Add ability to store and list tasks
    Any input other than "list" or "bye" is stored in memory (fixed-size
    array, up to 100 tasks) and confirmed with "added: ...". The "list"
    command prints all stored tasks, numbered. Completes the Level-2
    requirement.

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -11,13 +11,24 @@ public class Rex {
         System.out.println("Hello! I'm Rex!");
         System.out.println("What can I do for you?");
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
             input = scanner.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon");
+        System.out.println("Bye. Hope to see you again soon!");
         scanner.close();
     }
 }
